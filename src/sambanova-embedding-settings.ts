1f97d0d13ca968855dfc1559c6a14478b7d8d281
Rodrigo Maldonado <rodrigo.maldonado@sambanovasystems.com>
2025-08-22 18:54:44 -0500

    embeddings options updated

--- src/sambanova-embedding-settings.ts
+++
@@ -1,8 +0,0 @@
-export type SambaNovaEmbeddingModelId =
-  | 'E5-Mistral-7B-Instruct'
-  | (string & {});
-
-export interface SambaNovaEmbeddingSettings {
-  maxEmbeddingsPerCall?: number;
-  truncate?: boolean;
-}
